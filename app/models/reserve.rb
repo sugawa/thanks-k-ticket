644013bd98f1afd0707ccc3977d619e1157f0382
sugawa <hs0316@gmail.com>
2014-07-07 08:14:56 +0900

    スマホ対応・エラーメッセージ修正

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -2,7 +2,7 @@ class Reserve < ActiveRecord::Base
 	attr_accessor :email_confirmation
 	validates :name, presence: true, length: { maximum: 256 }
 	validates :kana, length: { maximum: 256 }
-	validates :email, presence: true, confirmation: true, email_format: true
+	validates :email, presence: true, confirmation: true, email_format: { message: "が一致しません。"}
 	validates :email_confirmation, presence: true
 	validates :number, presence: true, numericality: true, inclusion: { in: 1..10 }
 end
