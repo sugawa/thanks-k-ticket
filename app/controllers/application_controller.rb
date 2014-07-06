f06205c2cbe9f1a2b73bd74b0855b5a64bba7aad
sugawa <hs0316@gmail.com>
2014-07-06 23:27:32 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :basic_authenticate
+  def basic_authenticate
+    if Rails.env.development? || ENV['BASIC_AUTH'] == "ENABLED"
+      authenticate_or_request_with_http_basic('Secret Area') do |urlname, password|
+        [urlname, password] == ["kwww2","kannothx123"]
+      end
+    end
+  end
 end
