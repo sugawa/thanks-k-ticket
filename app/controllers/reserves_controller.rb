5f3e211549475ba0fcea8f40fb81c622486ef9f6
sugawa <hs0316@gmail.com>
2014-07-09 14:29:23 +0900

    fix

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -43,7 +43,7 @@ class ReservesController < ApplicationController
       stock = @settings.class.where("name = 'totals'").limit(1).pluck(:value) - @settings.class.where("name = 'members'").limit(1).pluck(:value)
       # 売れたチケットの枚数を取得
       sold = @reserve.class.sum(:number)
-      if stock.first > sold
+      if stock.first > to_int(sold)
         return true
       else
         return false
