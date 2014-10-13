644edd28680db076f15b78ca5565f76bdcefa824
sugawa <hs0316@gmail.com>
2014-10-13 14:36:44 +0900

    修正

--- app/mailers/mail_sender.rb
+++ app/mailers/mail_sender.rb
@@ -1,6 +1,6 @@
 # coding:utf-8
 class MailSender < ActionMailer::Base
-  default from: "ticket@thanks-k.com"
+  default from: "Thanks!Kオーケストラ <ticket@thanks-k.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -10,8 +10,8 @@ class MailSender < ActionMailer::Base
   def entry(reserve)
     @reserve = reserve
 
-    mail to: @reserve.email
-    mail bcc: "spearmint.hs@gmail.com"
-    mail subject: "[Thanks!Kオーケストラ][発券番号：#{@reserve.id}]ご予約ありがとうございました。"
+    mail :to => @reserve.email
+    mail :bcc => ["thanksk.orch.ticket@gmail.com", "hs0316@gmail.com"]
+    mail :subject => "[Thanks!Kオーケストラ第２回演奏会][発券番号：#{@reserve.id}]ご予約ありがとうございました。"
   end
 end
